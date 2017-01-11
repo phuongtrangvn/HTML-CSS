e6ba51b56346252d1a90c6fef0c42c669d14c592
trangdnp <phuongtrang.lovevnn@gmail.com>
2017-01-11 12:14:44 +0700

    BTVN fix Masonry & Add Dot

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -1,108 +1,61 @@
-// window.onload = function() {
-//   alert("hello!");
-// }
-var data = {
-  "items" : [
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "posterName"    : "Dzungggg",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/3.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/2.png",
-      "posterName"    : "Sannnn",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/2.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "posterName"    : "Tranggggg",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/4.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/3.png",
-      "posterName"    : "Ngannnnn",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "posterName"    : "Dzungggg",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/3.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/2.png",
-      "posterName"    : "Sannnn",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/2.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/1.png",
-      "posterName"    : "Tranggggg",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    },
-    {
-      "imageUrl"      : "http://www.vatcss.info/TechKidsGirls/4.png",
-      "view"          : 857,
-      "date"          : "07/05/12",
-      "plus"          : 588,
-      "posterAvatar"  : "http://www.vatcss.info/TechKidsGirls/3.png",
-      "posterName"    : "Ngannnnn",
-      "posterTitle"   : "HRC Photo",
-      "content"       : "Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores."
-    }
-  ]
-}
+var loadedData = [];
+var isRequestNextPage = false;
 
-$(document).ready(function() {
-  var source   = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-  $.ajax({
-    type: "get",
-    url : "imagesData.json"
-  }).then(function(data){
-    var html = template(data);
-    $('#item-list').html(html);
-    $('#item-list').masonry({
-      itemSelector: ".item_container",
-      columnWidth: ".item_container",
-      percentPosition: true
-    })
-  }).fail(function(error) {
-    console.log("Error : ");
-    console.log(error);
+$(document).ready(function(){
+  var itemTemplate = Handlebars.compile($("#item-template").html());
+  var itemModalTemplate = Handlebars.compile($("#item-modal-template").html());
+
+  requestNextPage(itemTemplate);
+
+  $('body').on('click', '.plus_button', function(){
+    var itemId = $(this).attr('data-item-id');
+
+    for(var i=0; i< loadedData.length; i++){
+      var itemData = loadedData[i];
+      if(itemData.id == itemId){
+        // Found item, populating data
+        $("#item_modal_body").html(itemModalTemplate(itemData));
+        break;
+      }
+    }
+
+    $("#item_modal").modal("show");
   });
 
-})
+  $(window).on('scroll', function(){
+    if (isRequestNextPage) return;
+    if($(window).scrollTop() + window.innerHeight > $(document).height() - 200){
+      setTimeout(function(){
+        requestNextPage(itemTemplate);
+      }, 1500);
+      isRequestNextPage = true;
+    }
+  });
+});
+
+function requestNextPage(itemTemplate){
+  $.ajax({
+    type  : "get",
+    url   : "imagesData.json"
+  }).then(function(data){
+    loadedData = loadedData.concat(data.items);
+
+    var $itemHtml = $(itemTemplate(data));
+    //$("#item_list").append(itemHtml);
+    $('#item_list').append($itemHtml).masonry( 'appended', $itemHtml );
+    $('#item_list').masonry({
+      itemSelector: '.item_container',
+      columnWidth: '.item_container',
+      percentPosition: true
+    });
+    // $('#item_list').masonry({
+    //   itemSelector: '.item_container',
+    //   columnWidth: '.item_container',
+    //   percentPosition: true
+    // });
+  }).fail(function(error){
+    console.log(error);
+  }).always(function(){
+    isRequestNextPage = false;
+  });
+}
